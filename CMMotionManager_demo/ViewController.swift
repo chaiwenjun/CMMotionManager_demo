b4a464e2a12da154b20a5fab5c88efb5188e312f
zhangzhaopds <zhangzhaopds@foxmail.com>
2017-01-17 15:35:42 +0800

    Initial Commit

---
+++ CMMotionManager_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CMMotionManager_demo
+//
+//  Created by 张昭 on 17/01/2017.
+//  Copyright © 2017 Jmsp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
